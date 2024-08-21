621b3d5c04f63e5d62c5ed3e14eafeb037992baf
Aravind1000 <aravind30052003@gmail.com>
2024-08-21 22:40:06 +0530

    Admin folder structure

--- Admin/AdminIntro.js
+++ Admin/AdminIntro.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Form, Input, message } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
-import { ShowLoading, HideLoading } from '../../redux/rootSlice';
+import { ShowLoading, HideLoading } from './redux/rootSlice';
 import axios from 'axios';
 
-function AdminContact() {
+function AdminIntro() {
     const dispatch = useDispatch();
     const { portfolioData } = useSelector((state) => state.root);
 
     const onFinish = async (values) => {
         try {
             dispatch(ShowLoading());
-            const response = await axios.post("/api/portfolio/update-contact", {
+            const response = await axios.post("/api/portfolio/update-intro", {
                 ...values,
-                _id: portfolioData.contact._id,
+                _id: portfolioData.intro._id,
             });
             dispatch(HideLoading());
             if (response.data.success) {
@@ -32,25 +32,25 @@ function AdminContact() {
             <Form
                 onFinish={onFinish}
                 layout='vertical'
-                initialValues={portfolioData.contact}
+                initialValues={portfolioData.intro}
             >
-                <Form.Item name='name' label='Name'>
-                    <Input placeholder='Name' />
+                <Form.Item name='welcomeText' label='Welcome Text'>
+                    <Input placeholder='Welcome Text' />
                 </Form.Item>
-                <Form.Item name='gender' label='Gender'>
-                    <Input placeholder='Gender' />
+                <Form.Item name='firstName' label='First Name'>
+                    <Input placeholder='First Name' />
                 </Form.Item>
-                <Form.Item name='email' label='E-mail'>
-                    <Input placeholder='E-mail' />
+                <Form.Item name='lastName' label='Last Name'>
+                    <Input placeholder='Last Name' />
                 </Form.Item>
-                <Form.Item name='mobile' label='Mobile'>
-                    <Input placeholder='Mobile' />
+                <Form.Item name='caption' label='Caption'>
+                    <Input placeholder='Caption' />
                 </Form.Item>
-                <Form.Item name='age' label='Age'>
-                    <Input placeholder='Age' />
+                <Form.Item name='description' label='Description'>
+                    <Input.TextArea placeholder='Description' />
                 </Form.Item>
-                <Form.Item name='address' label='Address'>
-                    <Input placeholder='Address' />
+                <Form.Item name='resume' label='Resume'>
+                    <Input placeholder='Resume link' />
                 </Form.Item>
                 <div className='flex justify-end w-full'>
                     <button className='px-10 py-2 bg-secondary text-white' type='submit'>
@@ -62,4 +62,4 @@ function AdminContact() {
     )
 }
 
-export default AdminContact;
+export default AdminIntro;
--- Admin/index.js
+++ Admin/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import Header from '../../components/Header'
+import Header from '../client/src/components/Header'
 import { Tabs } from 'antd';
 import AdminIntro from './AdminIntro';
 import AdminAbout from './AdminAbout';
