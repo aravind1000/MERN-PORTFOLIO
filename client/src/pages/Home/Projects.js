0373249324d17ec6e9b86d9fbdbcca119eaf3649
Aravind A <aravind30052003@gmail.com>
2026-01-04 10:44:38 +0530

    Refactor Projects component structure and styling

--- client/src/pages/Home/Projects.js
+++ client/src/pages/Home/Projects.js
@@ -4,16 +4,16 @@ import { useSelector } from 'react-redux';
 import { DarkModeContext } from '../../contexts/DarkModeContext';
 
 function Projects() {
-    const [selectedItemIndex, setSelectedItemIndex] = React.useState(0);
+    const [selectedItemIndex, setSelectedItemIndex] = React. useState(0);
     const { portfolioData } = useSelector((state) => state.root);
     const { projects = [] } = portfolioData; 
 
     const { darkMode } = useContext(DarkModeContext);
 
     return (
-        <div id='projects' className={`h-screen sm:h-auto flex flex-col justify-center ${darkMode ? 'bg-darkBg' : 'bg-white'}`}>
+        <div id='projects' className={`h-screen sm:h-auto flex flex-col justify-center ${darkMode ?  'bg-darkBg' : 'bg-white'}`}>
             <SectionTitle title="Projects" />
-            <div className={`flex py-10 gap-20 ${darkMode ? 'text-gray-300' : 'text-gray-700'} sm:flex-col`} >
+            <div className={`flex py-10 gap-20 ${darkMode ?  'text-gray-300' : 'text-gray-700'} sm:flex-col`} >
                 <div className={`flex flex-col gap-10 border-l-2 ${darkMode ? 'border-gray-600' : 'border-[#135e4c82]'} w-1/3 sm:flex-row sm:overflow-x-scroll sm:w-full`}>
                     {projects.map((project, index) => (
                         <div
@@ -22,7 +22,7 @@ function Projects() {
                             className='cursor-pointer'
                             aria-label={`Select project: ${project.title}`} 
                         >
-                            <h1 className={`text-xl px-5 ${selectedItemIndex === index ? 'text-tertiary border-tertiary border-l-4 -ml-[3px] bg-[#1a7f5a31]' : (darkMode ? 'text-gray-300' : 'text-gray-500')}`}>
+                            <h1 className={`text-xl px-5 ${selectedItemIndex === index ? 'text-tertiary border-tertiary border-l-4 -ml-[3px] bg-[#1a7f5a31]' : (darkMode ? 'text-gray-300' : 'text-gray-[...]
                                 {project.title}
                             </h1>
                         </div>
@@ -32,12 +32,6 @@ function Projects() {
                 <div className={`flex items-center justify-center gap-10 sm:flex-col`}>
                     {projects[selectedItemIndex] && (
                         <>
-                            <img
-                                draggable="false"
-                                src={projects[selectedItemIndex].image || 'default-image.png'}
-                                alt={projects[selectedItemIndex].title || 'Project Image'}
-                                className={`h-80 w-80 object-cover rounded-lg shadow-lg ${darkMode ? 'opacity-80' : ''}`}
-                            />
                             <div className='flex flex-col gap-5'>
                                 <h1 className={`text-secondary text-xl font-semibold ${darkMode ? 'text-gray-300' : ''}`}>
                                     {projects[selectedItemIndex].title || 'Title not available'}
