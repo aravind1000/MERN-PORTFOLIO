9111e2df2e45f0000de2a09df78f4e7a46d93b93
Aravind A <aravind30052003@gmail.com>
2026-01-04 11:43:10 +0530

    Refactor Projects component for better readability

--- client/src/pages/Home/Projects.js
+++ client/src/pages/Home/Projects.js
@@ -4,16 +4,16 @@ import { useSelector } from 'react-redux';
 import { DarkModeContext } from '../../contexts/DarkModeContext';
 
 function Projects() {
-    const [selectedItemIndex, setSelectedItemIndex] = React. useState(0);
+    const [selectedItemIndex, setSelectedItemIndex] = React.useState(0);
     const { portfolioData } = useSelector((state) => state.root);
     const { projects = [] } = portfolioData; 
 
     const { darkMode } = useContext(DarkModeContext);
 
     return (
-        <div id='projects' className={`h-screen sm:h-auto flex flex-col justify-center ${darkMode ?  'bg-darkBg' : 'bg-white'}`}>
+        <div id='projects' className={`h-screen sm:h-auto flex flex-col justify-center ${darkMode ? 'bg-darkBg' : 'bg-white'}`}>
             <SectionTitle title="Projects" />
-            <div className={`flex py-10 gap-20 ${darkMode ?  'text-gray-300' : 'text-gray-700'} sm:flex-col`} >
+            <div className={`flex py-10 gap-20 ${darkMode ? 'text-gray-300' : 'text-gray-700'} sm:flex-col`} >
                 <div className={`flex flex-col gap-10 border-l-2 ${darkMode ? 'border-gray-600' : 'border-[#135e4c82]'} w-1/3 sm:flex-row sm:overflow-x-scroll sm:w-full`}>
                     {projects.map((project, index) => (
                         <div
@@ -22,7 +22,7 @@ function Projects() {
                             className='cursor-pointer'
                             aria-label={`Select project: ${project.title}`} 
                         >
-                            <h1 className={`text-xl px-5 ${selectedItemIndex === index ? 'text-tertiary border-tertiary border-l-4 -ml-[3px] bg-[#1a7f5a31]' : (darkMode ? 'text-gray-300' : 'text-gray-[...]
+                            <h1 className={`text-xl px-5 ${selectedItemIndex === index ? 'text-tertiary border-tertiary border-l-4 -ml-[3px] bg-[#1a7f5a31]' : (darkMode ? 'text-gray-300' : 'text-gray-500')}`}>
                                 {project.title}
                             </h1>
                         </div>
